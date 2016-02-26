a7e4219d5aad7551c8410659a5b26dac7b50d1b0
Edd Robinson <me@edd.io>
2016-02-26 18:34:49 +0000

    Clean up on panic, and add build tag

--- clustertest/cluster_suite_test.go
+++ clustertest/cluster_suite_test.go
@@ -1,3 +1,5 @@
+// +build cluster
+
 package clustertest
 
 import (
@@ -7,18 +9,24 @@ import (
 	"testing"
 )
 
-// clst is not threadsafe. It is assumed (for the moment) that it is
-// setup in TestMain, prior to access from multiple goroutines in tests,
-// and that goroutines only concurrenly read from the cluster.
+// NOTE: clst is not threadsafe. It is assumed (for the moment) that it
+// is setup in TestMain, prior to access from multiple goroutines in
+// tests, and that goroutines only concurrenly read clst, and call
+// methods defined on the Cluster interface, rather than methods defined
+// on the underlying type.
 var clst Cluster
 
 func TestMain(m *testing.M) {
 	binPath := flag.String("bin", "", "Location of influxd binary")
+	hybridN := flag.Int("hybrid", 3, "Number of hybrid nodes in the cluster")
+	metaN := flag.Int("meta", 0, "Number of consensus nodes in the cluster")
+	dataN := flag.Int("data", 0, "Number of data nodes in the cluster")
 	flag.Parse()
 
 	var err error
-	// Let's create a cluster of 3 hybrid nodes.
-	if clst, err = newlocal(3, 0, 0, *binPath); err != nil {
+	// Let's create a cluster of 3 hybrid nodes. Here we could switch
+	// this out for a remote cluster.
+	if clst, err = newlocal(*hybridN, *metaN, *dataN, *binPath); err != nil {
 		panic(err)
 	}
 
@@ -32,10 +40,18 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-// TestDropDatabase tests that dropping a database results in the
-// database being dropped from all nodes in the cluster.
-func TestDropDatabase(t *testing.T) {
+func checkPanic(t *testing.T) {
+	if r := recover(); r != nil {
+		t.Logf("Panic in test detected: %v", r)
+		clst.Close()
+	}
+}
+
+// TestShowDropDatabase tests that a database is available on all data
+// nodes, and that when it's dropped, it's dropped from all data nodes.
+func TestShowDropDatabase(t *testing.T) {
 	t.Parallel()
+	defer checkPanic(t)
 
 	dbName, err := clst.NewDatabase()
 	if err != nil {
@@ -52,6 +68,7 @@ func TestDropDatabase(t *testing.T) {
 		if !result.HasDatabase(dbName) {
 			t.Fatalf("Node %d does not have database %s", resp.nodeID, dbName)
 		}
+		t.Logf("Node %d has database %s", resp.nodeID, dbName)
 	}
 
 	// When the database is dropped, it should be dropped on all nodes.
@@ -70,6 +87,36 @@ func TestDropDatabase(t *testing.T) {
 			t.Errorf("Node %d still has database %s", resp.nodeID, dbName)
 		}
 	}
+}
+
+func TestShowDropMeasurements(t *testing.T) {
+	t.Parallel()
+	defer checkPanic(t)
+	t.Skip("Not implemented yet")
+}
+
+func TestShowDropSeries(t *testing.T) {
+	t.Parallel()
+	defer checkPanic(t)
+	t.Skip("Not implemented yet")
+}
+
+func TestShowTagKeys(t *testing.T) {
+	t.Parallel()
+	defer checkPanic(t)
+	t.Skip("Not implemented yet")
+}
+
+func TestShowTagValues(t *testing.T) {
+	t.Parallel()
+	defer checkPanic(t)
+	t.Skip("Not implemented yet")
+}
+
+func TestFieldKeys(t *testing.T) {
+	t.Parallel()
+	defer checkPanic(t)
+	t.Skip("Not implemented yet")
 }
 
 func fmat(f string, v ...interface{}) string {
